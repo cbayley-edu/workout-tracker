074a9a6c2454ce85481d7773f1b24bb1a0211980
christina-edu <christina-edu@users.noreply.github.com>
2020-09-20 13:38:46 -0400

    models and server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// require("./routes/api-routes")(app);
+// require("./routes/html-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
