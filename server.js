cbb83fa84dc11db2175acd6dc640a0509c8e45d1
christina-edu <christina-edu@users.noreply.github.com>
2020-09-20 22:54:27 -0400

    updated server

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useFindAndModify: false
+ });
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
